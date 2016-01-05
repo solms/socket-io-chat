0694148b27a232c3c2ce4897d3363e13bbfcf1cd
Solms Dykman <solmsd@gmail.com>
2016-01-05 14:22:32 +0900

    Functional! ...but simple

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+var jquery = require('jquery');
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+
 app.use('/css', express.static('css'));
 app.use('/js',  express.static('js'));
 
@@ -14,10 +17,8 @@ var server = app.listen(port, function(){
 });
 
 var io = require('socket.io')(server);
-
 io.on('connection', function(socket){
-  console.log('A user connected.');
-  socket.on('disconnect', function(){
-   console.log('user disconnected');
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
   });
 });
